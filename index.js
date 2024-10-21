d47107c0d58270b09660fd80ad99a8bb20bdff8e
pharm2data <mollystephens321@gmail.com>
2024-10-21 16:24:29 -0400

    Java Script Tutorial
    Set up a page with HTML, Java Script, and CSS. Add IDs and use them with JavaScript. Create console log notes and alerts for the website.

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello")
+console.log("This is how to put info into the console log.")
+
+window.alert("This is how you generate an alert!")
+window.alert("You can create more than one alert!")
+
+document.getElementById("myH1").textContent = "Welcome!";
+document.getElementById('myP').textContent = "To the Farm!"
+
+// these are how you create comments that no one can see!
+
+/*  
+    you
+    can
+    create
+    more 
+    than 
+    one 
+    line 
+    comments
+
+    you still can't see them!
+*/
